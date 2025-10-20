becc7ea22231ef80e34bfd7ccd7e423bb1b2a372
leanmarqs <leanmarqs@gmail.com>
2025-10-20 10:59:23 -0300

    feat: tailwind initial config

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  
+  return (
+    <>
+      <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-gray-200 p-4">
+        Hello World!
+      </h1>
+    </div>
+
+    </>
+  )
+}
+
+export default App
